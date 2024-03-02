96cc9c0b87d078439493f7b3a24263afd991b737
IndigoCandle <romyannay1@gmail.com>
2024-03-02 02:59:09 +0200

    animation added

--- src/animation/MapVisualizer.java
+++ src/animation/MapVisualizer.java
@@ -6,10 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
-import javafx.scene.text.Text;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.effect.DropShadow;
+
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
